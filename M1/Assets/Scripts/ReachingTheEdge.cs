e6ae56676f71ef52282091ca731935e689602b08
DeanPack <deanpackard.94@gmail.com>
2017-10-10 20:00:23 -0600

    Porcupine Sprites and implementation
    I added porcupines!

---
+++ M1/Assets/Scripts/ReachingTheEdge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachingTheEdge : MonoBehaviour {
+
+	public bool turn= false;
+	void OnCollisionExit2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Platform")
+		{
+			turn = true;
+			Debug.Log("WOAH!");
+		}
+		Debug.Log("WOAH!");
+	}
+}
